aba23a354398bc5a6fc6dd761588522eb7df353b
Erin Olsen <erin.angelita@gmail.com>
2018-02-14 15:37:09 -0700

    converted to babel

--- server.js
+++ server.js
@@ -1,71 +1,71 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var mongoose = require('mongoose');
-var Bear = require('./public/bear.js');
+import express from 'express';
+import bodyParser from 'body-parser';
+const app = express();
+import mongoose from 'mongoose';
+import Bear from './public/bear.js';
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080
-var router = express.Router();
+const port = process.env.PORT || 8080;
+const router = express.Router();
 
-router.use(function(req, res, next) {
+router.use((req, res, next) => {
   console.log('Something is happening.');
   next();
 });
 
 mongoose.connect('mongodb://localhost/bears');
 router.route('/bears')
-.post(function(req, res) {
-  var bear = new Bear();
-  bear.name = req.body.name;
-  bear.save(function(err) {
-    if(err)
-    res.send(err);
+  .post(({body}, res) => {
+    const bear = new Bear();
+    bear.name = body.name;
+    bear.save(err => {
+      if(err)
+        res.send(err);
       res.json({message: 'Bear created!'});
     });
   })
-  .get(function(req, res) {
-    Bear.find(function(err, bears) {
+  .get((req, res) => {
+    Bear.find((err, bears) => {
       if (err)
         res.send(err);
-        res.json(bears);
-      });
+      res.json(bears);
     });
+  });
 router.route('/bears/:bear_id')
-  .get(function(req, res) {
-    Bear.findById(req.params.bear_id, function(err, bear) {
+  .get(({params}, res) => {
+    Bear.findById(params.bear_id, (err, bear) => {
       if(err)
         res.send(err);
       res.json(bear);
     });
   })
-.put(function(req, res) {
-  Bear.findById(req.params.bear_id, function(err, bear) {
-    if (err)
-      res.send(err);
-    bear.name = req.body.name;
-    bear.save(function(err) {
+  .put(({params, body}, res) => {
+    Bear.findById(params.bear_id, (err, bear) => {
       if (err)
         res.send(err);
-      res.json({message: 'Bear updated!'});
+      bear.name = body.name;
+      bear.save(err => {
+        if (err)
+          res.send(err);
+        res.json({message: 'Bear updated!'});
+      });
+    });
+  })
+  .delete(({params}, res) => {
+    Bear.remove( {
+      _id: params.bear_id
+    }, (err, bear) => {
+      if (err)
+        res.send(err);
+      res.json({ message: 'Successfully deleted'});
     });
   });
-})
-.delete(function(req, res) {
-  Bear.remove( {
-    _id: req.params.bear_id
-  }, function (err, bear){
-    if (err)
-      res.send(err);
-    res.json({ message: 'Successfully deleted'});
-  });
-});
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   res.json({message: 'horray! welcome to our api!'
   });
 });
 app.use('/api', router);
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log(`Magic happens on port ${port}`);
